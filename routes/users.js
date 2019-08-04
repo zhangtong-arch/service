b6d96a9f2f6d3a6c0bf496bfbaed3ab44734ddca
zhangtong-arch <1016128911@qq.com@example.com>
2019-08-04 21:18:21 +0800

    updata

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,12 @@
 const router = require('koa-router')()
-
+const UserController = require('../controllers/user');
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'zhang'
-})
+router.post('/create',UserController.create);
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+//获取所有用户
+router.get('/getUesrAll',UserController.getUesrAll)
+router.get('/deleteUserById',UserController.deleteUserById)
+router.post('/updataUserById',UserController.updataUserById)
 
 module.exports = router
